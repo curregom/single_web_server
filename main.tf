dbfbb0d8d5e2f34bfea325942a3280ecb8ed519f
Carlos <carlos.urrego@gmail.com>
2024-05-23 20:25:10 -0400

    single server

--- main.tf
+++ main.tf
@@ -3,15 +3,32 @@ provider "aws"{
     
     }
 
+variable "web_server_port" {
+    description = "port used by the web server"
+    type = number
+    default = 8080
+  
+}
+
+resource "aws_security_group" "web_server"{
+    ingress {
+        from_port = var.web_server_port
+        to_port = var.web_server_port
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
 resource "aws_instance" "lws01" {
     ami = "ami-04b70fa74e45c3917"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [ aws_security_group.web_server.id ]
 
 user_data = <<-EOF
 
     #!/bin/bash
     echo "Hello, World" > index.xhtml
-    nohup busybox httpd -f -p 8080 &
+    nohup busybox httpd -f -p "${var.web_server_port}" &
     EOF
 
 user_data_replace_on_change = true
@@ -19,5 +36,7 @@ user_data_replace_on_change = true
 tags = {
   Name = "single web server aws"
 }
+
+
 }
 

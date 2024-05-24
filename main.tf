99563fb997de4e2a3dc37434e6aad8d5aecdc274
Carlos <carlos.urrego@gmail.com>
2024-05-23 20:00:11 -0400

    first commit

--- main.tf
+++ main.tf
@@ -6,4 +6,18 @@ provider "aws"{
 resource "aws_instance" "lws01" {
     ami = "ami-04b70fa74e45c3917"
     instance_type = "t2.micro"
+
+user_data = <<-EOF
+
+    #!/bin/bash
+    echo "Hello, World" > index.xhtml
+    nohup busybox httpd -f -p 8080 &
+    EOF
+
+user_data_replace_on_change = true
+
+tags = {
+  Name = "single web server aws"
 }
+}
+
